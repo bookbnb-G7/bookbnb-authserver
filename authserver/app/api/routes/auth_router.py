3f89b47e968b998a3ce3e51753175a0e067dfa52
facutorraca <ftorraca@fi.uba.ar>
2020-12-06 00:44:15 -0300

    fix identation

--- authserver/app/api/routes/auth_router.py
+++ authserver/app/api/routes/auth_router.py
@@ -6,4 +6,4 @@ router = APIRouter()
 @router.post('/sign-in', status_code=200)
 async def sign_in(acces_token: Optional[str] = Header(None)):
 	check_token(acces_token)
-    return {'message': 'ok'}
+	return {'message': 'ok'}
