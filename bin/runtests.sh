22dc65791e2cd6f4618f59c4bd2507463fc53379
facutorraca <ftorraca@fi.uba.ar>
2021-01-19 01:46:34 -0300

    add github action and other thing

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 docker-compose up -d
-docker exec bookbnb-authserver_web bash -c 'while !</dev/tcp/db/5432; do sleep 1; done;'
-docker exec bookbnb-authserver_web pytest --cov=authserver --color=yes
 
-if [ ${1-"none"} == "no-lint" ]; then
-	docker-compose down
-else
-	docker exec bookbnb-authserver_web pylint authserver
-	docker-compose down
-fi
+echo "controlando inicio servidor";
+
+until curl -o /dev/null -s --connect-timeout 1 'http://localhost:3000';
+do
+    echo "Esperando 1 seg(s) a que el servidor se inicie";
+    sleep 1;
+done;
+
+docker exec bookbnb-authserver_web pytest --cov=authserver --color=yes
+docker-compose down
 
